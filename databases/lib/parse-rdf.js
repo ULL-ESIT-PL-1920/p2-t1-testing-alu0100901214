0f59835f9221e3119b9f92bd167332d2c0e449bc
Sergio González Guerra <alu0100901214@ull.edu.es>
2020-03-11 19:34:24 +0000

    El test para buscar el título, pasa

--- databases/lib/parse-rdf.js
+++ databases/lib/parse-rdf.js
@@ -4,5 +4,6 @@ module.exports = rdf => {
     const $ =cheerio.load(rdf);
     const book = {};
     book.id=+$('pgterms\\:ebook').attr('rdf:about').replace('ebooks/','');
+    book.title=$('dcterms\\:title').text();
 	return book;
 };
