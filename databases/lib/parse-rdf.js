a15df4082d8adb418c94d2aff4be3e3df660d3d2
Sergio González Guerra <alu0100901214@ull.edu.es>
2020-03-11 19:36:43 +0000

    El test para el array de autores, pasa

--- databases/lib/parse-rdf.js
+++ databases/lib/parse-rdf.js
@@ -5,5 +5,7 @@ module.exports = rdf => {
     const book = {};
     book.id=+$('pgterms\\:ebook').attr('rdf:about').replace('ebooks/','');
     book.title=$('dcterms\\:title').text();
+    book.authors=$('pgterms\\:agent	pgterms\\:name')
+	.toArray().map(elem=>$(elem).text());
 	return book;
 };
